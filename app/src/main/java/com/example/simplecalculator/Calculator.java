2be2ea6badca44b442d6afb151fcbc2d6e30efbf
Adrian <rdrgzadr@gmail.com>
2022-07-12 11:00:35 +0800

    added selectable buttons and decimal functionality

--- app/src/main/java/com/example/simplecalculator/Calculator.java
+++ app/src/main/java/com/example/simplecalculator/Calculator.java
@@ -22,53 +22,38 @@ public class Calculator extends AppCompatActivity {
         ((TextView)findViewById(R.id.welcome)).setText("WELCOME, " + greeting);
     }
 
-    public void addition (View v){
-        Intent abcd = getIntent();
-        String greeting = abcd.getStringExtra("username");
-        String num1 = ((EditText)findViewById(R.id.firstdigit)).getText().toString();
-        int number1 = Integer.parseInt(num1);
-        String num2 = ((EditText)findViewById(R.id.seconddigit)).getText().toString();
-        int number2 = Integer.parseInt(num2);
-        int sum = number1 + number2;
-        ((TextView)findViewById(R.id.answer)).setText(greeting + ", answer is: " + sum);
+    int operation = 0;
 
+    public void addition (View v){
+        operation = 1;
+        findViewById(R.id.plus).setEnabled(false);
+        findViewById(R.id.minus).setEnabled(true);
+        findViewById(R.id.multiply).setEnabled(true);
+        findViewById(R.id.divide).setEnabled(true);
     }
 
     public void subtraction (View v){
-        Intent abcd = getIntent();
-        String greeting = abcd.getStringExtra("username");
-        String num1 = ((EditText)findViewById(R.id.firstdigit)).getText().toString();
-        int number1 = Integer.parseInt(num1);
-        String num2 = ((EditText)findViewById(R.id.seconddigit)).getText().toString();
-        int number2 = Integer.parseInt(num2);
-        int diff = number1 - number2;
-        ((TextView)findViewById(R.id.answer)).setText(greeting + ", answer is: " + diff);
-
-
+        operation = 2;
+        findViewById(R.id.plus).setEnabled(true);
+        findViewById(R.id.minus).setEnabled(false);
+        findViewById(R.id.multiply).setEnabled(true);
+        findViewById(R.id.divide).setEnabled(true);
     }
 
     public void multiplication (View v){
-        Intent abcd = getIntent();
-        String greeting = abcd.getStringExtra("username");
-        String num1 = ((EditText)findViewById(R.id.firstdigit)).getText().toString();
-        int number1 = Integer.parseInt(num1);
-        String num2 = ((EditText)findViewById(R.id.seconddigit)).getText().toString();
-        int number2 = Integer.parseInt(num2);
-        int prod = number1 * number2;
-        ((TextView)findViewById(R.id.answer)).setText(greeting + ", answer is: " + prod);
-
-
+        operation = 3;
+        findViewById(R.id.plus).setEnabled(true);
+        findViewById(R.id.minus).setEnabled(true);
+        findViewById(R.id.multiply).setEnabled(false);
+        findViewById(R.id.divide).setEnabled(true);
     }
 
     public void division (View v){
-        Intent abcd = getIntent();
-        String greeting = abcd.getStringExtra("username");
-        String num1 = ((EditText)findViewById(R.id.firstdigit)).getText().toString();
-        int number1 = Integer.parseInt(num1);
-        String num2 = ((EditText)findViewById(R.id.seconddigit)).getText().toString();
-        int number2 = Integer.parseInt(num2);
-        int quot = number1 / number2;
-        ((TextView)findViewById(R.id.answer)).setText(greeting + ", answer is: " + quot);
+        operation = 4;
+        findViewById(R.id.plus).setEnabled(true);
+        findViewById(R.id.minus).setEnabled(true);
+        findViewById(R.id.multiply).setEnabled(true);
+        findViewById(R.id.divide).setEnabled(false);
     }
 
     public void reset (View v){
@@ -85,5 +70,37 @@ public class Calculator extends AppCompatActivity {
         findViewById(R.id.buttonbianx).setEnabled(false);
         ((Button)findViewById(R.id.buttonbianx)).setText("BLUE");
         ((Button)findViewById(R.id.buttonbianx)).setBackgroundColor(Color.parseColor("#0000FF"));
+    }
+
+    public void compute (View v)
+    {
+        Intent abcd = getIntent();
+        String greeting = abcd.getStringExtra("username");
+        String num1 = ((EditText)findViewById(R.id.firstdigit)).getText().toString();
+        float number1 = Float.parseFloat(num1);
+        String num2 = ((EditText)findViewById(R.id.seconddigit)).getText().toString();
+        float number2 = Float.parseFloat(num2);
+
+    switch (operation)
+    {
+        case 0:
+            Toast.makeText(this,"Please select operation", Toast.LENGTH_SHORT).show();
+        case 1:
+            float sum = number1 + number2;
+            ((TextView)findViewById(R.id.answer)).setText(greeting + ", answer is: " + sum);
+            break;
+        case 2:
+            float diff = number1 - number2;
+            ((TextView)findViewById(R.id.answer)).setText(greeting + ", answer is: " + diff);
+            break;
+        case 3:
+            float prod = number1 * number2;
+            ((TextView)findViewById(R.id.answer)).setText(greeting + ", answer is: " + Math.abs(prod));
+            break;
+        case 4:
+            float quot = number1 / number2;
+            ((TextView)findViewById(R.id.answer)).setText(greeting + ", answer is: " + quot);
+            break;
+    }
     }
 }
